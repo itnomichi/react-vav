8dcf79299a69bc5adfbb934ef66d7544adba8bd0
itnomichi <itnomichi@gmail.com>
2018-09-19 03:25:02 +0000

    original

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const App = () => {
+    return (
+        <div align="center">
+            <h1>Welcome Reacter.</h1>
+        </div>
+    );
+};
+
+export default App;
